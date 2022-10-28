29fc4be50a09190897f98e8d26b1087b4be90867
Moritz Schiesser <moritz@moritzschiesser.ch>
2022-10-28 14:31:22 +0200

    implement and document simplay backend

---
+++ src/simplay/src/simplay/events.py
@@ -0,0 +1,235 @@
+from typing import Union
+from simpy.resources.container import ContainerAmount
+from simpy.core import SimTime
+
+
+class VisualEvent:
+    """
+    Base class for all visual events.
+    """
+
+    def __init__(self, for_id: str, timestamp: SimTime, action: str, **kwargs: dict):
+        """
+        :param for_id: The id of the component this event is for.
+        :param timestamp: The timestamp of the event.
+        :param action: The action to be performed.
+        :param kwargs: The arguments for the action.
+        """
+        self.for_id = for_id
+        self.timestamp = timestamp
+        self.action = action
+        self.kwargs = kwargs
+
+
+class SetVisible(VisualEvent):
+    """
+    Event to set the visibility of a component.
+    """
+
+    def __init__(self, for_id: str, timestamp: SimTime, visible: bool):
+        """
+        :param for_id: The id of the component this event is for.
+        :param timestamp: The timestamp of the event.
+        :param visible: Whether the component should be visible or not.
+        """
+        super().__init__(for_id, timestamp, "SET_VISIBLE", visible=visible)
+
+
+class SetPosition(VisualEvent):
+    """
+    Event to set the position of a component.
+    """
+
+    def __init__(self, for_id: str, timestamp: SimTime, x: int, y: int):
+        """
+        :param for_id: The id of the component this event is for.
+        :param timestamp: The timestamp of the event.
+        :param x: The x coordinate of the component.
+        :param y: The y coordinate of the component.
+        """
+        super().__init__(for_id, timestamp, "SET_POSITION", x=x, y=y)
+
+
+class SetInteracting(VisualEvent):
+    """
+    Event to set the interaction of a component.
+    """
+
+    def __init__(self, for_id: str, timestamp: SimTime, with_id: str):
+        """
+        :param for_id: The id of the component this event is for.
+        :param timestamp: The timestamp of the event.
+        :param with_id: The id of the component the first component is interacting with.
+        """
+        super().__init__(for_id, timestamp, "SET_INTERACTING", with_id=with_id)
+
+
+class SetNotInteracting(VisualEvent):
+    """
+    Event to set the interaction of a component.
+    """
+
+    def __init__(self, for_id: str, timestamp: SimTime, with_id: str):
+        """
+        :param for_id: The id of the component this event is for.
+        :param timestamp: The timestamp of the event.
+        :param with_id: The id of the component the first component is interacting with.
+        """
+        super().__init__(for_id, timestamp, "SET_NOT_INTERACTING", with_id=with_id)
+
+
+class MoveNear(VisualEvent):
+    """
+    Event to move a component near another component.
+    """
+
+    def __init__(self, for_id: str, timestamp: SimTime, target_id: str):
+        """
+        :param for_id: The id of the component this event is for.
+        :param timestamp: The timestamp of the event.
+        :param target_id: The id of the component the first component should move near.
+        """
+        super().__init__(for_id, timestamp, "MOVE_NEAR", target_id=target_id)
+
+
+class MoveNearPoint(VisualEvent):
+    """
+    Event to move a component near a point.
+    """
+
+    def __init__(self, for_id: str, timestamp: SimTime, x: int, y: int):
+        """
+        :param for_id: The id of the component this event is for.
+        :param timestamp: The timestamp of the event.
+        :param x: The x coordinate of the point.
+        :param y: The y coordinate of the point.
+        """
+        super().__init__(for_id, timestamp, "MOVE_NEAR_POINT", x=x, y=y)
+
+
+class SetTintColor(VisualEvent):
+    """
+    Event to set the tint color of a component.
+    """
+
+    def __init__(self, for_id: str, timestamp: SimTime, color: int):
+        """
+        :param for_id: The id of the component this event is for.
+        :param timestamp: The timestamp of the event.
+        :param color: The color to tint the component.
+        """
+        super().__init__(for_id, timestamp, "SET_TINT_COLOR", color=color)
+
+
+class SetDecoratingText(VisualEvent):
+    """
+    Event to set the decorating text of a component.
+    """
+
+    def __init__(self, for_id: str, timestamp: SimTime, text: str):
+        """
+        :param for_id: The id of the component this event is for.
+        :param timestamp: The timestamp of the event.
+        :param text: The text to set as decorating text.
+        """
+        super().__init__(for_id, timestamp, "SET_DECORATING_TEXT", text=text)
+
+
+class SetSpriteFrame(VisualEvent):
+    """
+    Event to set the sprite frame of a component.
+    """
+
+    def __init__(self, for_id: str, timestamp: SimTime, frame: int):
+        """
+        :param for_id: The id of the component this event is for.
+        :param timestamp: The timestamp of the event.
+        :param frame: The frame to set the sprite to.
+        """
+        super().__init__(for_id, timestamp, "SET_SPRITE_FRAME", frame=frame)
+
+
+class ResourceSetCapacity(VisualEvent):
+    """
+    Event to set the capacity of a resource.
+    """
+
+    def __init__(self, for_id: str, timestamp: SimTime, capacity: int):
+        """
+        :param for_id: The id of the component this event is for.
+        :param timestamp: The timestamp of the event.
+        :param capacity: The capacity to set the resource to.
+        """
+        super().__init__(for_id, timestamp, "RESOURCE.SET_CAPACITY", capacity=capacity)
+
+
+class ResourceSetUtilization(VisualEvent):
+    """
+    Event to set the utilization of a resource.
+    """
+
+    def __init__(self, for_id: str, timestamp: SimTime, utilization: int):
+        """
+        :param for_id: The id of the component this event is for.
+        :param timestamp: The timestamp of the event.
+        :param utilization: The utilization to set the resource to.
+        """
+        super().__init__(
+            for_id, timestamp, "RESOURCE.SET_UTILIZATION", utilization=utilization
+        )
+
+
+class ContainerSetCapacity(VisualEvent):
+    """
+    Event to set the capacity of a container.
+    """
+
+    def __init__(self, for_id: str, timestamp: SimTime, capacity: int):
+        """
+        :param for_id: The id of the component this event is for.
+        :param timestamp: The timestamp of the event.
+        :param capacity: The capacity to set the container to.
+        """
+        super().__init__(for_id, timestamp, "CONTAINER.SET_CAPACITY", capacity=capacity)
+
+
+class ContainerSetLevel(VisualEvent):
+    """
+    Event to set the level of a container.
+    """
+
+    def __init__(self, for_id: str, timestamp: SimTime, level: ContainerAmount):
+        """
+        :param for_id: The id of the component this event is for.
+        :param timestamp: The timestamp of the event.
+        :param level: The level to set the container to.
+        """
+        super().__init__(for_id, timestamp, "CONTAINER.SET_LEVEL", level=level)
+
+
+class StoreSetCapacity(VisualEvent):
+    """
+    Event to set the capacity of a store.
+    """
+
+    def __init__(self, for_id: str, timestamp: SimTime, capacity: Union[float, int]):
+        """
+        :param for_id: The id of the component this event is for.
+        :param timestamp: The timestamp of the event.
+        :param capacity: The capacity to set the store to.
+        """
+        super().__init__(for_id, timestamp, "STORE.SET_CAPACITY", capacity=capacity)
+
+
+class StoreSetContent(VisualEvent):
+    """
+    Event to set the content of a store.
+    """
+
+    def __init__(self, for_id: str, timestamp: SimTime, content):
+        """
+        :param for_id: The id of the component this event is for.
+        :param timestamp: The timestamp of the event.
+        :param content: The content to set the store to.
+        """
+        super().__init__(for_id, timestamp, "STORE.SET_CONTENT", content=content)
---
+++ src/simplay/src/simplay/visualization.py
@@ -0,0 +1,158 @@
+from typing import List
+import jsons
+
+from components import VisualComponent, VisualEnvironment
+from events import VisualEvent
+
+
+class VisualGrid:
+    """
+    This class represents a grid for the visualization.
+    """
+
+    def __init__(self, width: int, height: int, cols: int, rows: int):
+        """
+        :param width: The width of the grid in pixels.
+        :param height: The height of the grid in pixels.
+        :param cols: The number of columns in the grid.
+        :param rows: The number of rows in the grid.
+        """
+        self.width = width
+        self.height = height
+        self.cols = cols
+        self.rows = rows
+        self.areas = []
+
+    def set_area(
+        self, id: str, name: str, height: int, width: int, x: int, y: int, color: int
+    ):
+        """
+        Set an area in the grid.
+
+        :param id: The id of the area.
+        :param name: The name of the area.
+        :param height: The height of the area in rows.
+        :param width: The width of the area in columns.
+        :param x: The x coordinate of the top left corner of the area.
+        :param y: The y coordinate of the top left corner of the area.
+        :param color: The color of the area.
+        """
+        self.areas.append(
+            {
+                "id": id,
+                "name": name,
+                "color": color,
+                "gridDefinition": {"height": height, "width": width, "x": x, "y": y},
+            }
+        )
+
+
+class VisualizationManager:
+    """
+    This class acts as a central point for all entities, visuals, sprites and events.
+    """
+
+    def __init__(self, env: VisualEnvironment):
+        """
+        :param env: The simulation environment that is visualized.
+        """
+        self.env = env
+        """
+        The simulation environment that is visualized.
+        """
+        self.events = []
+        """
+        The events that happened in the simulation.
+        """
+        self.entities = []
+        """
+        The entities which exist in the simulation.
+        """
+        self.visuals = []
+        """
+        The visuals that have been registered with the manager.
+        """
+        self.sprites = []
+        """
+        The sprites that have been registered with the manager.
+        """
+        self.grid = None
+        """
+        The grid that is used for the visualization.
+        """
+
+    def add_entity(self, entity: VisualComponent, type: str):
+        """
+        Add an entity to the visualization.
+
+        :param entity: The entity to add.
+        :param type: The type of the entity, one of ``'PROCESS'``, ``'RESOURCE'``, ``'CONTAINER'``, ``'STORE'``, ``'CUSTOM'``.
+        """
+        self.entities.append(
+            {
+                "id": entity.id,
+                "type": type,
+                "visual": entity.graphic,
+                "tint": entity.tint,
+            }
+        )
+
+    def add_event(self, event: VisualEvent):
+        """
+        Add an event to the visualization.
+
+        :param event: The event to add.
+        """
+        self.events.append(event)
+        self.events.sort(key=lambda x: x.timestamp)
+
+    def register_visual(self, id: str, visual: str):
+        """
+        Register a visual with the manager.
+
+        :param id: The id of the visual, it must be unique and can be used to reference the graphic in components.
+        :param visual: The path to the visual.
+        """
+        self.visuals.append({"id": id, "visual": visual})
+
+    def register_sprite(self, id: str, frames: List[str]):
+        """
+        Register a sprite with the manager.
+
+        :param id: The id of the sprite, it must be unique and can be used to reference the graphic in components.
+        :param frames: A list of paths to the frames of the sprite.
+        """
+        self.sprites.append({"id": id, "frames": frames})
+
+    def set_grid(self, grid: VisualGrid):
+        """
+        Set the grid for the visualization.
+
+        :param grid: The grid to use.
+        """
+        self.grid = grid
+
+    def serialize(self) -> str:
+        """
+        Serialize the visualization to a JSON string.
+        """
+        return jsons.dumps(
+            {
+                "events": self.events,
+                "entities": self.entities,
+                "visuals": self.visuals,
+                "sprites": self.sprites,
+                "grid": self.grid,
+            },
+            strip_privates=True,
+            key_transformer=jsons.KEY_TRANSFORMER_CAMELCASE,
+        )
+
+    def write_to_file(self, filename: str):
+        """
+        Write the visualization to a file.
+
+        :param filename: The name of the file to write to.
+        """
+        with open(filename, "w") as f:
+            f.write(self.serialize())
