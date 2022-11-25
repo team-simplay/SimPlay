31e8691aa75347c405ca15109378db41226088f7
Moritz Schiesser <moritz@moritzschiesser.ch>
2022-11-25 09:11:20 +0100

    remove test idea

--- src/simplay-web/test/EventFactory.test.ts
+++
@@ -1,8 +0,0 @@
-import { EventFactory } from '../src/EventFactory';
-import { expect } from 'chai';
-
-// describe('EventFactory tests', function () {
-//   it('should initialize correctly', () => {
-
-//   });
-// });
