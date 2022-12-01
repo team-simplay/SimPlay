526bc689259d5de99b41860d676aaacea6c4e38a
David Kühnhanss <david.kuehnhanss@gmail.com>
2022-12-01 11:14:06 +0100

    generated frontend extension with extension-cookiecutter-ts

--- src/simplay-jupyter/src/index.ts
+++ src/simplay-jupyter/src/index.ts
@@ -1,5 +1,17 @@
-// Copyright (c) team-simplay
-// Distributed under the terms of the Modified BSD License.
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
 
-export * from './version';
-export * from './widget';
+/**
+ * Initialization data for the simplay_jupyter extension.
+ */
+const plugin: JupyterFrontEndPlugin<void> = {
+  id: 'simplay_jupyter:plugin',
+  autoStart: true,
+  activate: (app: JupyterFrontEnd) => {
+    console.log('JupyterLab extension simplay_jupyter is activated!');
+  }
+};
+
+export default plugin;
