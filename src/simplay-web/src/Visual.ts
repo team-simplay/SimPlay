b0580581a7161ba40cf8d30eae868ba01884ccda
Moritz Schiesser <moritz@moritzschiesser.ch>
2022-12-01 13:56:52 +0100

    implement preloading images which hopefully helps

--- src/simplay-web/src/Visual.ts
+++ src/simplay-web/src/Visual.ts
@@ -4,3 +4,11 @@ export interface Visual {
   id: string;
   frames: string[];
 }
+
+export async function preLoadImages(visuals: Visual[]) {
+  for (const visual of visuals) {
+    for (const frame of visual.frames) {
+      Assets.backgroundLoad(frame);
+    }
+  }
+}
