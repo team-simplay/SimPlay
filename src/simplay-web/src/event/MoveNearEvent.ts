c4c69a41f4564e3ca45726cd5133518aebaf3d49
Moritz Schiesser <moritz@moritzschiesser.ch>
2022-12-02 11:17:33 +0100

    implement logic, tests

--- src/simplay-web/src/event/MoveNearEvent.ts
+++ src/simplay-web/src/event/MoveNearEvent.ts
@@ -2,6 +2,7 @@ import { MoveNearEventArgs } from './MoveNearEventArgs';
 import { Event } from './Event';
 import { EventAction } from './EventAction';
 import { SimplayContext } from '../SimplayContext';
+import { getEntityDisplayObjectById } from '../Entity';
 
 export class MoveNearEvent extends Event {
   constructor(
@@ -12,6 +13,22 @@ export class MoveNearEvent extends Event {
     super(forId, timestamp, EventAction.MOVE_NEAR, args);
   }
   execute(context: SimplayContext) {
-    throw new Error('Method not implemented.');
+    const entityDisplayObject = getEntityDisplayObjectById(context, this.forId);
+    const targetDisplayObject = getEntityDisplayObjectById(
+      context,
+      this.args.target
+    );
+    const x = targetDisplayObject.x + (Math.random() - 0.5) * context.tileWidth;
+    const y =
+      targetDisplayObject.y + (Math.random() - 0.5) * context.tileHeight;
+
+    const entityWidth = entityDisplayObject.getBounds().width;
+    const entityHeight = entityDisplayObject.getBounds().height;
+
+    const xAdjust = (entityWidth - context.tileWidth) / 2;
+    const yAdjust = (entityHeight - context.tileHeight) / 2;
+
+    entityDisplayObject.x = x - xAdjust;
+    entityDisplayObject.y = y - yAdjust;
   }
 }
--- src/simplay-web/test/event/SetPositionEvent.test.ts
+++ src/simplay-web/test/event/SetPositionEvent.test.ts
@@ -4,13 +4,17 @@ import { SetPositionEventArgs } from '../../src/event/SetPositionEventArgs';
 import { EventAction } from '../../src/event/EventAction';
 import { getTestGrid } from './getTestGrid';
 import { SimulationDataSerialized } from '../../src/SimulationDataSerialized';
-import { mock, instance } from 'ts-mockito';
+import { mock, instance, spy, when } from 'ts-mockito';
 import { SimulationSpooler } from '../../src/SimulationSpooler';
+import * as PIXI from 'pixi.js';
 
 const forId = 'leet';
 const timestamp = 1337;
 
-describe('SetPositionEvent tests', function () {
+const fromUrlSpy = spy(PIXI.Texture);
+when(fromUrlSpy.fromURL('leet.png')).thenResolve(PIXI.Texture.WHITE);
+
+describe('SetPositionEvent tests', async function () {
   const simulationDataSerialized = {
     entities: [
       {
@@ -49,14 +53,16 @@ describe('SetPositionEvent tests', function () {
     expect(event.args.y).to.equal(0);
   });
 
-  it('should execute correctly', () => {
+  it('should execute correctly', async () => {
     const args = new SetPositionEventArgs({ x: 1, y: 1 });
     const event = new SetPositionEvent(forId, timestamp, args);
     const containerMock = mock(HTMLDivElement);
+    when(containerMock.clientHeight).thenReturn(500);
+    when(containerMock.clientWidth).thenReturn(500);
     const container = instance(containerMock);
 
     const spooler = new SimulationSpooler(simulationDataSerialized, container);
-
+    await new Promise((resolve) => setTimeout(resolve, 100));
     event.execute(spooler.context);
     expect(spooler.context.entityContainer.children[0].x).to.equal(
       spooler.context.tileWidth + spooler.context.tileWidth / 2
