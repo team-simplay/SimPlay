14288292f5debc9ce24db0dff70bd32131f4e48a
Moritz Schiesser <moritz@moritzschiesser.ch>
2022-12-06 10:48:04 +0100

    implement and test

--- src/simplay-web/src/Entity.ts
+++ src/simplay-web/src/Entity.ts
@@ -15,10 +15,20 @@ export interface Entity {
   tint: number;
 }
 
+export interface ContainerEntity extends Entity {
+  type: 'CONTAINER';
+  capacity: number;
+  level: number;
+}
+
 export interface DisplayEntity {
   animatedSprite: PIXI.AnimatedSprite;
   decoratingText: PIXI.Text;
   container: PIXI.Container;
+}
+
+export interface ExtendedDisplayEntity extends DisplayEntity {
+  informationText: PIXI.Text;
 }
 
 export function getEntityDisplayObjectById(
@@ -53,13 +63,30 @@ export async function createEntities(context: SimplayContext) {
     container.addChild(sprite);
     container.addChild(text);
     container.visible = false;
-    context.entityContainer.addChild(container);
 
-    context.entityDictionary[entity.id] = {
+    let displayEntity = {
       animatedSprite: sprite,
       decoratingText: text,
       container: container,
-    };
+    } as DisplayEntity;
+
+    if (
+      entity.type === 'CONTAINER' ||
+      entity.type === 'STORE' ||
+      entity.type === 'RESOURCE'
+    ) {
+      const informationText = createInformationText(entity);
+      informationText.y = sprite.y + sprite.height + 6;
+      informationText.x = sprite.x + sprite.width / 2;
+      container.addChild(informationText);
+      displayEntity = {
+        ...displayEntity,
+        informationText: informationText,
+      } as ExtendedDisplayEntity;
+    }
+
+    context.entityContainer.addChild(container);
+    context.entityDictionary[entity.id] = displayEntity;
   }
 }
 
@@ -72,6 +99,19 @@ function createDecoratingText(entity: Entity): PIXI.Text {
   });
   text.anchor.set(0.5, 0.5);
   text.name = `${entity.id}-text`;
+  return text;
+}
+
+function createInformationText(entity: Entity): PIXI.Text {
+  const text = new PIXI.Text(entity.id, {
+    fontFamily: 'Arial',
+    fontSize: 12,
+    fill: 0xffffff,
+    align: 'center',
+  });
+  text.anchor.set(0.5, 0.5);
+  text.name = `${entity.id}-text-information`;
+  text.text = '';
   return text;
 }
 
