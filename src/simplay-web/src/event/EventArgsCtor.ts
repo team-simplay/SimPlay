dc81b8c3c91cdef2f83c1c1031a52baf1a56a4a0
Moritz Schiesser <moritz@moritzschiesser.ch>
2022-12-07 11:46:38 +0100

    resolve first warnings

--- src/simplay-web/src/event/EventArgsCtor.ts
+++ src/simplay-web/src/event/EventArgsCtor.ts
@@ -1,3 +1,5 @@
 import { EventArgs } from './EventArgs';
 
+// this eslint-disable is needed because the derived classes have different types for their args
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type EventArgsCtor<T extends EventArgs> = new (args: any) => T;
