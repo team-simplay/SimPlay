fb510b3962be30fd6eae58a2999a3b237d47c2b2
Moritz Schiesser <moritz@moritzschiesser.ch>
2022-12-10 09:52:52 +0100

    work through review

--- src/simplay-web/src/event/InteractionLine.ts
+++ src/simplay-web/src/event/InteractionLine.ts
@@ -31,52 +31,66 @@ export class InteractionLine {
   }
 
   redraw() {
+    const interactionLineSegments = 21;
     this.graphic.clear();
     this.createGradientLine(
       this.sourceEntity.container.x + this.sourceEntity.container.width / 2,
       this.sourceEntity.container.y + this.sourceEntity.container.height / 2,
       this.targetEntity.container.x + this.targetEntity.container.width / 2,
       this.targetEntity.container.y + this.targetEntity.container.height / 2,
-      21,
+      interactionLineSegments,
       0x000000,
       0xffffff
     );
   }
 
-  createGradientLine(
+  createStartingSegment(
     fromX: number,
     fromY: number,
-    toX: number,
-    toY: number,
-    stops: number,
     color1: number,
     color2: number
   ) {
-    const deltaX = (toX - fromX) / stops;
-    const deltaY = (toY - fromY) / stops;
-    this.offsetX += deltaX / 50;
-    this.offsetY += deltaY / 50;
     this.graphic.lineStyle(2, color2, 0.8);
     if (this.switch) {
       this.graphic.lineStyle(2, color1, 0.8);
     }
     this.graphic.moveTo(fromX, fromY);
     this.graphic.lineTo(this.offsetX + fromX, this.offsetY + fromY);
-    for (let i = 0; i < stops - 1; i++) {
-      let color = i % 2 === 0 ? color1 : color2;
-      if (this.switch) {
-        color = i % 2 === 0 ? color2 : color1;
-      }
-      this.graphic.lineStyle(2, color, 0.8);
-      this.graphic.moveTo(
-        this.offsetX + fromX + deltaX * i,
-        +this.offsetY + fromY + deltaY * i
-      );
-      this.graphic.lineTo(
-        this.offsetX + fromX + deltaX * (i + 1),
-        this.offsetY + fromY + deltaY * (i + 1)
-      );
+  }
+
+  createInnerSegment(
+    color1: number,
+    color2: number,
+    deltaX: number,
+    deltaY: number,
+    fromX: number,
+    fromY: number,
+    i: number
+  ) {
+    let color = i % 2 === 0 ? color1 : color2;
+    if (this.switch) {
+      color = i % 2 === 0 ? color2 : color1;
     }
+    this.graphic.lineStyle(2, color, 0.8);
+    this.graphic.moveTo(
+      this.offsetX + fromX + deltaX * i,
+      +this.offsetY + fromY + deltaY * i
+    );
+    this.graphic.lineTo(
+      this.offsetX + fromX + deltaX * (i + 1),
+      this.offsetY + fromY + deltaY * (i + 1)
+    );
+  }
+
+  createEndSegment(
+    toX: number,
+    toY: number,
+    deltaX: number,
+    deltaY: number,
+    stops: number,
+    color1: number,
+    color2: number
+  ) {
     if (this.offsetX > deltaX || this.offsetY > deltaY) {
       this.offsetX = 0;
       this.offsetY = 0;
@@ -94,5 +108,27 @@ export class InteractionLine {
       toY - (deltaY - this.offsetY)
     );
     this.graphic.lineTo(toX, toY);
+  }
+
+  createGradientLine(
+    fromX: number,
+    fromY: number,
+    toX: number,
+    toY: number,
+    stops: number,
+    color1: number,
+    color2: number
+  ) {
+    const lineMovementSpeedFactor = 50;
+    const deltaX = (toX - fromX) / stops;
+    const deltaY = (toY - fromY) / stops;
+    this.offsetX += deltaX / lineMovementSpeedFactor;
+    this.offsetY += deltaY / lineMovementSpeedFactor;
+
+    this.createStartingSegment(fromX, fromY, color1, color2);
+    for (let i = 0; i < stops - 1; i++) {
+      this.createInnerSegment(color1, color2, deltaX, deltaY, fromX, fromY, i);
+    }
+    this.createEndSegment(toX, toY, deltaX, deltaY, stops, color1, color2);
   }
 }
